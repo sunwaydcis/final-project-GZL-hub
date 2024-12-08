e70c195a5ddb42ffd7bb6bee9ac0e11044ce50bc
Goo Zong Lin <zonglingoo@gmail.com>
2024-12-09 00:56:13 +0800

    Add PlayerController and debugging
    Assign FXML ID to mainmenu.fxml

---
+++ src/main/scala/ch/makery/address/view/PlayerUIController.scala
@@ -0,0 +1,18 @@
+package ch.makery.address.controller
+
+import javafx.fxml.FXML
+import javafx.scene.control.TextArea
+
+class PlayerUIController {
+
+  @FXML
+  private var locationLog: TextArea = _
+
+  @FXML
+  private var goodsLog: TextArea = _
+
+  @FXML
+  private var profitLossLog: TextArea = _
+
+  // test
+}
