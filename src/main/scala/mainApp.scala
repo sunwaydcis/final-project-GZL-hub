b27f584212d29f98ef95d658247d083b06a19c03
Goo Zong Lin <zonglingoo@gmail.com>
2024-12-08 23:29:38 +0800

    Player UI implementation
    Add a simple concept which will developed more detail later

---
+++ src/main/scala/mainApp.scala
@@ -0,0 +1,15 @@
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.scene.layout.VBox
+import javafx.fxml.FXMLLoader
+
+object MyApp extends JFXApp3:
+
+  override def start(): Unit =
+    val root = FXMLLoader.load(getClass.getResource("playerUI.fxml"))
+    stage = new PrimaryStage:
+      title = "Trading Game"
+      scene = new Scene(root)
+
+end MyApp
