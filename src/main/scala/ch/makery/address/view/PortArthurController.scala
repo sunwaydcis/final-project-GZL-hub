17f877e9b9fcf29194eeec7cef2db553148928e6
Goo Zong Lin <zonglingoo@gmail.com>
2025-01-08 05:00:32 +0800

    characterStatsupload

--- src/main/scala/ch/makery/address/view/PortArthurController.scala
+++ src/main/scala/ch/makery/address/view/PortArthurController.scala
@@ -3,9 +3,10 @@ package ch.makery.address.view
 import javafx.fxml.{FXML, FXMLLoader}
 import javafx.scene.Parent
 import javafx.scene.control.{Button, Label}
+import javafx.scene.image.{Image, ImageView}
 import javafx.stage.Stage
 import ch.makery.address.controller.MarketController
-import ch.makery.address.model.GameState
+import ch.makery.address.model.{Character, GameState, SelectedCharacter}
 
 class PortArthurController {
 
@@ -14,6 +15,48 @@ class PortArthurController {
 
   @FXML
   private var marketPricesLabel: Label = _
+
+  @FXML
+  private var cashLabel: Label = _
+
+  @FXML
+  private var bankLabel: Label = _
+
+  @FXML
+  private var debtLabel: Label = _
+
+  @FXML
+  private var cargoSizeLabel: Label = _
+
+  @FXML
+  private var avatarImageView: ImageView = _
+
+  private var selectedCharacter: Option[Character] = _
+
+  def initialize(): Unit = {
+    // Retrieve the selected character from the singleton object
+    selectedCharacter = SelectedCharacter.character
+    updateCharacterStats()
+  }
+
+  private def updateCharacterStats(): Unit = {
+    selectedCharacter.foreach { character =>
+      cashLabel.setText(s"Cash: ${character.cash}")
+      bankLabel.setText(s"Bank: ${character.bank}")
+      debtLabel.setText(s"Debt: ${character.debt}")
+      cargoSizeLabel.setText(s"Cargo Size: ${character.caravan.currentSize}/${character.caravan.maxSize}")
+      // Set the avatar image
+      val imagePath = s"/assets/${character.name.toLowerCase}.png" // Use character name for the image path
+      val imageStream = getClass.getResourceAsStream(imagePath)
+      println(s"Loading image from path: $imagePath")
+      if (imageStream != null) {
+        val avatarImage = new Image(imageStream)
+        avatarImageView.setImage(avatarImage)
+      } else {
+        println(s"Image not found: $imagePath")
+      }
+    }
+  }
 
   @FXML
   private def handleGoToBank(): Unit = {
